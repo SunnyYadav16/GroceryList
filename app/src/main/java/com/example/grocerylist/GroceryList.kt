edbccfc1c6913d3362e8514978fade71446ce4ff
Sunny Yadav <67675772+SunnyYadav16@users.noreply.github.com>
2024-09-16 10:35:48 -0400

    Update GroceryList.kt

--- app/src/main/java/com/example/grocerylist/GroceryList.kt
+++ app/src/main/java/com/example/grocerylist/GroceryList.kt
@@ -1,5 +1,8 @@
 package com.example.grocerylist
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,8 +10,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,6 +28,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 data class ShoppingItemData(
@@ -34,6 +45,14 @@ fun GroceryShopApp() {
 
     var showDialog by remember {
         mutableStateOf(false)
+    }
+
+    var itemName by remember {
+        mutableStateOf("")
+    }
+
+    var itemQty by remember {
+        mutableStateOf("")
     }
 
     Column(
@@ -54,38 +73,68 @@ fun GroceryShopApp() {
                 .fillMaxSize()
                 .padding(16.dp)
         ) {
-
+           items(sItems) {
+               currentItem ->
+               if (currentItem.isEditing) {
+                   GroceryItemEditor(item = currentItem, onEditComplete = {n, q -> {}})
+               } else {
+                   GroceryItemListView(
+                       item = currentItem,
+                       onEditClick = {
+                   },
+                   onDeleteClick = {
+                       sItems = sItems - currentItem
+                   })
+               }
+           }
         }
     }
 
     if (showDialog) {
         AlertDialog(
-            onDismissRequest = { showDialog = false },
+            onDismissRequest = {},
             confirmButton = {
                 Row (
-                    modifier = Modifier.fillMaxWidth().padding(4.dp),
+                    modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceBetween
-                ) {
+                ){
                     // SUBMIT BUTTON
-                    Button(onClick = { /*TODO*/ }) {
+                    Button(
+                        onClick = {
+                            val myNewItem = ShoppingItemData (
+                                id = sItems.size + 1,
+                                name = itemName,
+                                quantity = itemQty.toInt()
+                            )
+
+                            sItems += myNewItem
+                            itemQty = ""
+                            itemName = ""
+                            showDialog = false
+                        }) {
                         Text(text = "Submit")
                     }
 
                     // CANCEL BUTTON
                     Button(
-                        onClick = { showDialog = false }
+                        onClick = {
+                            itemQty = ""
+                            itemName = ""
+                            showDialog = false
+                        }
                     ) {
                         Text(text = "Cancel")
                     }
                 }
             },
-
             title = { Text(text = "Add Grocery Items") },
             text = {
                 Column {
                     OutlinedTextField(
-                        value = "",
-                        onValueChange = {},
+                        value = itemName,
+                        onValueChange = {
+                            itemName = it
+                        },
                         label = { Text(text = "Item Name")},
                         singleLine = true,
                         modifier = Modifier
@@ -94,8 +143,10 @@ fun GroceryShopApp() {
                     )
 
                     OutlinedTextField(
-                        value = "",
-                        onValueChange = {},
+                        value = itemQty,
+                        onValueChange = {
+                            itemQty = it
+                        },
                         label = { Text(text = "Item Quantity")},
                         singleLine = true,
                         modifier = Modifier
@@ -110,7 +161,90 @@ fun GroceryShopApp() {
 
 
 
+@Composable
+fun GroceryItemListView(
+    item : ShoppingItemData,
+    onEditClick : () -> Unit,
+    onDeleteClick : () -> Unit
+) {
+
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .border(
+                border = BorderStroke(2.dp, Color.Red),
+                shape = RoundedCornerShape(20)
+            ),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+
+        Column {
+            Text(text = "Item: ${item.name}", modifier = Modifier.padding(8.dp))
+            Text(text = "Qty: ${item.quantity}", modifier = Modifier.padding(8.dp))
+        }
+
+        Row (modifier = Modifier.padding(15.dp)) {
+            IconButton(onClick = onEditClick) {
+                Icon(imageVector = Icons.Default.Edit, contentDescription = null)
+            }
+
+            IconButton(onClick = onDeleteClick) {
+                Icon(imageVector = Icons.Default.Delete, contentDescription = null)
+            }
+        }
+    }
+}
 
 
+@Composable
+fun GroceryItemEditor(
+    item : ShoppingItemData,
+    onEditComplete: (String, Int) -> Unit,
+) {
 
+    var editedName by remember {
+        mutableStateOf(item.name)
+    }
 
+    var editedQuantity by remember {
+        mutableStateOf(item.quantity.toString())
+    }
+
+    var isEditing by remember {
+        mutableStateOf(item.isEditing)
+    }
+
+    Row (
+        modifier = Modifier.background(Color.Yellow).padding(4.dp)
+    ) {
+        Column () {
+            OutlinedTextField(
+                value = editedName,
+                onValueChange = {
+                    editedName = it
+                },
+                singleLine = true,
+                modifier = Modifier.padding(8.dp)
+            )
+
+            OutlinedTextField(
+                value = editedQuantity,
+                onValueChange = {
+                    editedQuantity = it
+                },
+                singleLine = true,
+                modifier = Modifier.padding(8.dp)
+            )
+
+            Button(onClick = {
+                isEditing = false
+                onEditComplete(editedName, editedQuantity.toInt())
+            }) {
+                Text(text = "Save")
+            }
+        }
+
+    }
+
+}
